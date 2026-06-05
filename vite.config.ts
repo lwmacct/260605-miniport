01beae28faba1a16b6d92852631a03bbc93546c6
lwmacctw <lwmacct@icloud.com>
2026-06-05 23:07:07 +0800

    chore 2026-06-05 23:07:07 CST

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 5173,
+    port: 40237,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8080",
+        target: "http://127.0.0.1:40238",
         changeOrigin: true,
       },
     },
   },
 });
-
